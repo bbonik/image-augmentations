05b704b5963c7c0e7f02cb62b3c0d7e221cf523f
Vassilios Vonikakis <bbonik@gmail.com>
2021-04-16 12:07:06 +0800

    added json file for bboxes and 2nd test image

--- source/test_augmentation_single_image.py
+++ source/test_augmentation_single_image.py
@@ -7,7 +7,7 @@ bounding boxes.
 @author: vasileios vonikakis
 """
 
-
+import json
 from augmentations import augment_affine
 import matplotlib.pyplot as plt
 plt.close('all')
@@ -17,35 +17,24 @@ plt.close('all')
 if __name__=="__main__":
     
     
-    filename = "../images/dogs_1.jpg"
+    # open json file with bounding boxes (x,y,w,h)
+    with open('../images/bboxes.json', 'r') as json_file:
+       dc_bboxes = json.load(json_file)
+       
     
+    # get filename and list of bounding boxes
+    filename = dc_bboxes['image1']['filename']
+    ls_bboxes = dc_bboxes['image1']['bboxes']
     
-    # reference test bboxes
-    x1 = [320,805]
-    y1 = [404,1078]
-    w1 = x1[1] - x1[0]
-    h1 = y1[1] - y1[0]
-    
-    x2 = [888,1296]
-    y2 = [270,1078]
-    w2 = x2[1] - x2[0]
-    h2 = y2[1] - y2[0]
-    
-    x3 = [1300,1683]
-    y3 = [450,1094]
-    w3 = x3[1] - x3[0]
-    h3 = y3[1] - y3[0]
+    # filename = dc_bboxes['image2']['filename']
+    # ls_bboxes = dc_bboxes['image2']['bboxes']
     
     
     # getting augmented images
     image_augm = augment_affine(
         image_filename=filename,
         # bboxes = None,
-        bboxes = [
-            (x1[0], y1[0], w1, h1), 
-            (x2[0], y2[0], w2, h2), 
-            (x3[0], y3[0], w3, h3) 
-            ],
+        bboxes =ls_bboxes,
         how_many=10,
         random_seed=0,
         range_scale=(0.5, 1.5),  # percentage
@@ -59,8 +48,5 @@ if __name__=="__main__":
         bbox_discard_thr = 0.85,  # percentage
         verbose=True  # set as False if you are generating lots of images!!!
         )
-
-    
-    
     
     
